3ed6708ae894dfa076051bab71a3ee82cc54b397
Casper S <au602242@post.au.dk>
2020-05-30 21:15:37 +0200

    Initial Commit

---
+++ cronx/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  cronx
+//
+//  Created by Casper Sørensen on 30/05/2020.
+//  Copyright © 2020 Casper Sørensen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
